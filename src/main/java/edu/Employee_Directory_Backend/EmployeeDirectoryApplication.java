8509064a56e2003d8137850a73d584a6acaf9fe7
Kalum Gunawardhana <kguna415@gmail.com>
2025-04-29 10:07:18 +0530

    first commit

---
+++ src/main/java/edu/Employee_Directory_Backend/EmployeeDirectoryApplication.java
@@ -0,0 +1,13 @@
+package edu.Employee_Directory_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectoryApplication.class, args);
+	}
+
+}
