c3ed0540fcece50ffcc0b71cdeec5e9fa1dbb8b9
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-04 21:20:18 +0530

    commit

--- src/main/java/edu/Employee_Directory_Backend/service/EmployeeService.java
+++ src/main/java/edu/Employee_Directory_Backend/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package edu.Employee_Directory_Backend.service;
 
-import edu.Employee_Directory_Backend.dto.EmployeeDto;
+import edu.Employee_Directory_Backend.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
-
+    List<Employee> getAllEmployees();
+    Employee createEmployee(Employee employee);
+    Employee updateEmployee(Long id, Employee updatedEmployee);
+    void deleteEmployee(Long id);
 }
--- src/main/java/edu/Employee_Directory_Backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/edu/Employee_Directory_Backend/service/impl/EmployeeServiceImpl.java
@@ -1,15 +1,34 @@
 package edu.Employee_Directory_Backend.service.impl;
 
-import edu.Employee_Directory_Backend.dto.EmployeeDto;
 import edu.Employee_Directory_Backend.entity.Employee;
 import edu.Employee_Directory_Backend.repository.EmployeeRepository;
 import edu.Employee_Directory_Backend.service.EmployeeService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    @Override
+    public List<Employee> getAllEmployees() {
+        return List.of();
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+    }
 }
