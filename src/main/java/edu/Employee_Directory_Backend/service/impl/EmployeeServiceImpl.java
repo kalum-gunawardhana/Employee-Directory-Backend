c0b71a9728f769af2a80b52f60b515e6d51afb95
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-04 21:30:52 +0530

    commit

--- src/main/java/edu/Employee_Directory_Backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/edu/Employee_Directory_Backend/service/impl/EmployeeServiceImpl.java
@@ -14,21 +14,38 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> getAllEmployees() {
-        return List.of();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee createEmployee(Employee employee) {
-        return null;
+        if (employeeRepository.existsByEmail(employee.getEmail())){
+            throw new IllegalArgumentException("Email already exists");
+        }
+        return employeeRepository.save(employee);
     }
 
     @Override
     public Employee updateEmployee(Long id, Employee updatedEmployee) {
-        return null;
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Employee not found"));
+
+        if (!existingEmployee.getEmail().equals(updatedEmployee.getEmail()) &&
+                employeeRepository.existsByEmail(updatedEmployee.getEmail())){
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        existingEmployee.setName(updatedEmployee.getName());
+        existingEmployee.setEmail(updatedEmployee.getEmail());
+        existingEmployee.setDepartment(updatedEmployee.getDepartment());
+
+        return employeeRepository.save(existingEmployee);
     }
 
     @Override
     public void deleteEmployee(Long id) {
-
+        if (!employeeRepository.existsById(id)){
+            throw new IllegalArgumentException("Employee not found");
+        }
+        employeeRepository.deleteById(id);
     }
 }
