644ef9fd057aa540445240d32b819c6288311abd
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-04 21:13:51 +0530

    commit

--- src/main/java/edu/Employee_Directory_Backend/repository/EmployeeRepository.java
+++ src/main/java/edu/Employee_Directory_Backend/repository/EmployeeRepository.java
@@ -1,12 +1,10 @@
 package edu.Employee_Directory_Backend.repository;
 
 import edu.Employee_Directory_Backend.entity.Employee;
-import edu.Employee_Directory_Backend.enums.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    boolean existsByEmail(String email);
 }
